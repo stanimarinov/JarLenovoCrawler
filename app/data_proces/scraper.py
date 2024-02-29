876febdf30c43f11e36d648d875064e8467ec96b
Stani Marinov <stani.marinov@yahoo.com>
2024-02-29 17:30:30 +0200

    add scraper

---
+++ app/data_proces/scraper.py
@@ -0,0 +1,40 @@
+"""Module: scraper"""
+
+import logging
+from bs4 import BeautifulSoup
+
+format_str = '%(name)s:%(levelname)s:%(message)s'
+logging.basicConfig(format=format_str, level=logging.DEBUG)
+logger = logging.getLogger('scraper')
+
+class ScraperError(Exception):
+    """Custom exception for errors related to Scraper."""
+
+    def __init__(self, message: str):
+        super().__init__(message)
+
+class Scraper:
+    def __init__(self,html):
+        self.soup = BeautifulSoup(self.html,"html.parser")  
+
+    def get_products_urls(self):
+        products_urls = []
+
+        logger.info('Start scraping data')
+        products_selector = "#product_list .sProduct .s2"
+
+        products = self.soup.select(products_selector)
+        logger.debug('PRODUCTS: %s', products)
+        for product in products:
+            logger.debug('PRODUCT: %s', product)
+            # get brand
+            brand = product.select_one('.list_brand brand').text.strip()
+
+            if brand == 'Lenovo':
+                # get product link:
+                a = product.select_one('.long_title description>a')
+                href = a['href']
+                products_urls.append(href)
+
+        return products_urls
+         
