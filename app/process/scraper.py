60ad17cb9699f67cd1917c9d59d7da48795592c7
Stani Marinov <stani.marinov@yahoo.com>
2024-03-10 14:45:06 +0200

    add changes

--- app/process/scraper.py
+++ app/process/scraper.py
@@ -1,18 +1,19 @@
 """Module Scraper"""
 
-import logging
+import requests
 from bs4 import BeautifulSoup
+import logging
 
 format_str = '%(name)s:%(levelname)s:%(message)s'
 logging.basicConfig(format=format_str, level=logging.INFO)
 logger = logging.getLogger('scraper')
 
+url = 'https://www.jarcomputers.com/Laptopi_cat_2.html'
 
 class Scraper:
     def __init__(self, html:str):
         self.html = html
         self.soup = BeautifulSoup(self.html, "html.parser")
-
     
     def get_products_urls(self):
         products_urls = []
@@ -23,10 +24,17 @@ class Scraper:
 
         for product in products:
             brand = product.select_one('.brand-name>a')
-            logger.info('PRODUCT: %s', product)
-
+            logger.debug('PRODUCT: %s', product)
             if brand and brand.text == 'Lenovo':
                 href = product.find('a')['href']
-                products_urls.append(href)
-                
+                products_urls.append(href)                
         return products_urls
+    
+page = requests.get(url) 
+soup = BeautifulSoup(page.content, "html.parser") 
+products = soup.find_all('a') 
+text = 'Lenovo'
+ 
+for product in products:
+    if(product.string == text):
+        print(product)
